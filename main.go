d93a5dd92da322f16ed53ee7d97ad808b8185296
fatahnuram <fatahnuram@gmail.com>
2024-10-22 17:42:57 +0700

    feat: read file

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+var datafile = "./grind.csv"
 
 func main() {
-	fmt.Println("running...")
-	fmt.Println("processing...")
-	fmt.Println("done.")
+	file, err := os.Open(datafile)
+	if err != nil {
+		log.Fatalf("error opening file: %v, msg: %v", datafile, err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for i := 0; scanner.Scan(); {
+		fmt.Printf("line %d, content: %v\n", i, scanner.Text())
+		i++
+	}
+	if err = scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
